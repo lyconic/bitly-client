b934999ceb5a8bd8691cbdc827a4666bab760953
Aron Filbert <afilbert@lyconic.com>
2021-03-16 00:08:23 -0500

    Initial commit from scaffolding

---
+++ lib/bitly/client.rb
@@ -0,0 +1,7 @@
+require "bitly/client/version"
+
+module Bitly
+  module Client
+    # Your code goes here...
+  end
+end
---
+++ spec/bitly/client_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Bitly::Client do
+  it "has a version number" do
+    expect(Bitly::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
